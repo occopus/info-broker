7362b9fa06998eab9803790ee4db84c607cc17c7
Adam Visegradi <visegradi.adam@sztaki.mta.hu>
2014-06-13 13:25:06 +0200

    Add requirements.txt

--- setup.py
+++ setup.py
@@ -1,20 +1,20 @@
 #!/usr/bin/env -e python
 
-from distutils.code import setup
+from distutils.core import setup
+from pip.req import parse_requirements
+
+reqs = [ str(i.req) for i in parse_requirements('requirements.txt') ]
 
 setup(
-    name='TowelStuff',
+    name='InfoBroker',
     version='0.1.0',
-    author='J. Random Hacker',
-    author_email='jrh@example.com',
-    packages=['towelstuff', 'towelstuff.test'],
-    scripts=['bin/stowe-towels.py','bin/wash-towels.py'],
-    url='http://pypi.python.org/pypi/TowelStuff/',
+    author='Adam Visegradi',
+    author_email='adam.visegradi@sztaki.mta.hu',
+    packages=['occo.infobroker', 'occo.infobroker.test'],
+#    scripts=['bin/stowe-towels.py','bin/wash-towels.py'],
+    url='http://www.lpds.sztaki.hu/',
     license='LICENSE.txt',
-    description='Useful towel-related stuff.',
+    description='Information Broker',
     long_description=open('README.txt').read(),
-    install_requires=[
-        "Django >= 1.1.1",
-        "caldav == 0.1.4",
-    ],
+    install_requires=reqs,
 )
