37ed3d58c0f83a8731135fff285b9f097163cf25
Adam Visegradi <visegradi.adam@sztaki.mta.hu>
2014-09-04 14:04:45 +0200

    Implement Key-Value Store abstraction layer
    Also implement
    - trivial KV Store using dict() for testing
    - some simple test cases

---
+++ occo/infobroker/kvstore.py
@@ -0,0 +1,61 @@
+#
+# Copyright (C) 2014 MTA SZTAKI
+#
+# Key-Value store abstraction for the OCCO InfoBroker
+#
+
+__all__ = ['KeyValueStore',
+           'KeyValueStoreProvider',
+           'DictKVStore']
+
+import occo.infobroker as ib
+import occo.util.factory as factory
+import yaml
+import logging
+
+log = logging.getLogger('occo.infobroker.kvstore')
+
+class KeyValueStore(factory.MultiBackend):
+    def __init__(self, **kwargs):
+        pass
+
+    def query_item(self, key):
+        raise NotImplementedError()
+    def set_item(self, key, value):
+        raise NotImplementedError()
+    def has_key(self, key):
+        raise NotImplementedError()
+
+    def __getitem__(self, key):
+        return self.query_item(key)
+    def __setitem__(self, key, value):
+        return self.set_item(key, value)
+    def __contains__(self, key):
+        return self.has_key(key)
+
+@factory.register(KeyValueStore, 'dict')
+class DictKVStore(KeyValueStore):
+    def __init__(self, **kwargs):
+        self.backend = dict()
+
+    def query_item(self, key):
+        return self.backend[key]
+    def set_item(self, key, value):
+        self.backend[key] = value
+    def has_key(self, key):
+        return key in self.backend
+
+@ib.provider
+class KeyValueStoreProvider(ib.InfoProvider):
+    def __init__(self, backend):
+        self.backend = backend
+    def get(self, key):
+        if self._can_immediately_get(key):
+            return self._immediate_get(key)
+        else:
+            return self.backend.query_item(key)
+    def can_get(self, key):
+        return self._can_immediately_get(key) or self.backend.has_key(key)
+    @property
+    def iterkeys(self):
+        raise NotImplementedError()
---
+++ tests/unittests/kvstore.py
@@ -0,0 +1,66 @@
+#
+# Copyright (C) 2014 MTA SZTAKI
+#
+# Unit tests for the SZTAKI Cloud Orchestrator
+#
+
+import unittest
+from common import *
+import occo.util as util
+import occo.util.config as config
+import occo.infobroker.kvstore as kvs
+import threading
+import logging.config
+import uuid
+
+with open('test_kvstore.yaml') as f:
+    cfg = config.DefaultYAMLConfig(f)
+
+logging.config.dictConfig(cfg.logging)
+
+class KVSTest(unittest.TestCase):
+    def test_inst_good(self):
+        self.assertEqual(kvs.KeyValueStore(protocol='dict').__class__,
+                         kvs.DictKVStore)
+    def test_inst_bad(self):
+        with self.assertRaises(util.ConfigurationError): 
+            p = kvs.KeyValueStore(protocol='nonexistent')
+    def test_dict_set_1(self):
+        p = kvs.KeyValueStore(protocol='dict')
+        p.set_item('alma', 'korte')
+    def test_dict_get_1(self):
+        p = kvs.KeyValueStore(protocol='dict')
+        p.set_item('alma', 'korte')
+        self.assertEqual(p.query_item('alma'), 'korte')
+    def test_dict_haskey_1(self):
+        p = kvs.KeyValueStore(protocol='dict')
+        p.set_item('alma', 'korte')
+        self.assertTrue(p.has_key('alma'))
+    def test_dict_set_2(self):
+        p = kvs.KeyValueStore(protocol='dict')
+        p['alma'] = 'korte'
+    def test_dict_get_2(self):
+        p = kvs.KeyValueStore(protocol='dict')
+        p['alma'] = 'korte'
+        self.assertEqual(p['alma'], 'korte')
+    def test_dict_haskey_2(self):
+        p = kvs.KeyValueStore(protocol='dict')
+        p['alma'] = 'korte'
+        self.assertTrue('alma' in p)
+
+class ProviderTest(unittest.TestCase):
+    def setUp(self):
+        self.backend = kvs.KeyValueStore(protocol='dict')
+        self.p = kvs.KeyValueStoreProvider(self.backend)
+    def test_dict_set(self):
+        self.backend['alma'] = 'korte'
+    def test_dict_get(self):
+        self.backend['alma'] = 'korte'
+        self.assertEqual(self.p.get('alma'), 'korte')
+#    def test_dict_haskey(self):
+#        self.p['alma'] = 'korte'
+#        self.assertTrue('alma' in self.p)
+
+
+if __name__ == '__main__':
+    unittest.main()
