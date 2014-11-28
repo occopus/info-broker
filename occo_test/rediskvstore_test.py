e86ab60b47ee8f14460eb36e8d66fa0d52cf3cf8
Novak Adam <novak.adam@sztaki.mta.hu>
2014-11-28 12:34:24 +0000

    update rediskvstore test with set_item and query_item tests

--- occo_test/rediskvstore_test.py
+++ occo_test/rediskvstore_test.py
@@ -3,11 +3,15 @@ import occo.infobroker.kvstore as kvs
 import redis
 import yaml
 import occo.util as util
+import unittest
 
-def setup ():
-    global data
-    with open(util.rel_to_file("rediskvstore_demo.yaml"), 'r') as stream:
-	data=yaml.load(stream)
-def test_inst():
-    store=kvs.KeyValueStore(**data)
-    
+class RKVSTest(unittest.TestCase):
+    def setUp(self):
+	with open(util.rel_to_file("rediskvstore_demo.yaml"), 'r') as stream:
+	    self.data=yaml.load(stream)
+    def test_inst(self):
+	self.store=kvs.KeyValueStore(**self.data)
+    def test_setget(self):
+	self.store=kvs.KeyValueStore(**self.data)
+	self.store.set_item('alma', 'korte')
+	self.assertEqual(self.store.query_item('alma'), 'korte')
