db7e68b00d08905d908af0fddcf8dd2b9e83a6c4
Adam Visegradi <visegradi.adam@sztaki.mta.hu>
2015-04-07 10:28:41 +0000

    Optimization: remove key-prefix for non-default databases
    The key prefix would be the same for all items in the database, so it is
    a waste of resources (storage and lookup).

--- occo/infobroker/rediskvstore.py
+++ occo/infobroker/rediskvstore.py
@@ -31,18 +31,45 @@ class RedisConnectionData(object):
     def __eq__(self, other):
         return (self.host, self.port, self.db) == \
             (other.host, other.port, other.db)
+    def __str__(self):
+        return '{0}:{1}/{2}'.format(self.host, self.port, self.db)
 
 class RedisConnectionPools:
 
     connection_pools = dict()
 
     @staticmethod
-    def get(host, port, db):
-        data = RedisConnectionData(host, port, db)
+    def get(rcd):
         pools = RedisConnectionPools.connection_pools
-        if not data in pools:
-            pools[data] = redis.ConnectionPool(host=host, port=port, db=db)
-        return pools[data]
+        if not rcd in pools:
+            pools[rcd] = redis.ConnectionPool(
+                host=rcd.host, port=rcd.port, db=rcd.db)
+        return pools[rcd]
+
+class DBSelectorKey(object):
+    def __init__(self, key, kvstore):
+        dbname, newkey = self.splitkey(key)
+        if dbname in kvstore.altdbs:
+            self.db = kvstore.altdbs[dbname]
+            self.key = newkey
+        else:
+            self.db = kvstore.default_db
+            self.key = key
+        self.rcd = RedisConnectionData(kvstore.host, kvstore.port, self.db)
+
+    def splitkey(self, key):
+        parts = key.split(':', 1)
+        if len(parts) > 1:
+            return parts
+        return None, key
+
+    def get_connection(self):
+        conn = redis.StrictRedis(
+            connection_pool=RedisConnectionPools.get(self.rcd))
+        return conn, self.key
+
+    def __str__(self):
+        return '{0}::{1}'.format(self.rcd, self.key)
 
 @factory.register(kvs.KeyValueStore, 'redis')
 class RedisKVStore(kvs.KeyValueStore):
@@ -71,34 +98,21 @@ class RedisKVStore(kvs.KeyValueStore):
         self.serialize = serialize
         self.deserialize = deserialize
 
-    def get_db(self, dbname):
-        # This works with dbname=None too
-        return self.altdbs.get(dbname, self.default_db)
-
-    def open_connection(self, dbname=None):
-        connection_pool = RedisConnectionPools.get(
-            self.host, self.port, self.get_db(dbname))
-        return redis.StrictRedis(connection_pool=connection_pool)
-
-    def find_dbname(self, key):
-        parts = key.split(':', 1)
-        if len(parts) > 1:
-            return parts[0]
-        return None
-
-    def connection_by_key(self, key):
-        return self.open_connection(self.find_dbname(key))
+    def transform_key(self, key):
+        tkey = DBSelectorKey(key, self)
+        log.debug("Querying key %s", tkey)
+        return tkey.get_connection()
 
     def query_item(self, key, default=None):
-        backend = self.connection_by_key(key)
+        backend, key = self.transform_key(key)
         data = backend.get(key)
         retval = self.deserialize(data) if data else None
         return util.coalesce(retval, default)
 
     def set_item(self, key, value):
-        backend = self.connection_by_key(key)
+        backend, key = self.transform_key(key)
         backend.set(key, self.serialize(value) if value else None)
 
     def _contains_key(self, key):
-        backend = self.connection_by_key(key)
+        backend, key = self.transform_key(key)
         return self.backend.exists(key)
