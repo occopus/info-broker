cfd75cdeb8eba748154abe1ad00b82462067d64f
Adam Visegradi <visegradi.adam@sztaki.mta.hu>
2015-07-28 14:02:32 +0000

    Conditionally register the UDS instance globally
    The condition is True by default, as it is expected that there always
    will be only one UDS instance in an OCCO application.

--- occo/infobroker/uds.py
+++ occo/infobroker/uds.py
@@ -32,10 +32,14 @@ class UDS(ib.InfoProvider, factory.MultiBackend):
     It uses the :ref:`abstract factory <factory>` framework so backend-specific
     optimizations are possible.
 
+    :param bool main_uds: If :data:`True`, the instance will register itself in
+        :mod:`occo.infobroker` as the globally available main UDS instance.
 
     """
-    def __init__(self):
+    def __init__(self, main_uds=True):
         self.ib = ib.main_info_broker
+        if main_uds:
+            ib.real_main_uds = self
 
     def infra_key(self, infra_id):
         """
@@ -342,8 +346,8 @@ class UDS(ib.InfoProvider, factory.MultiBackend):
 
 @factory.register(UDS, 'dict')
 class DictUDS(UDS):
-    def __init__(self, **backend_config):
-        super(DictUDS, self).__init__()
+    def __init__(self, main_uds=True, **backend_config):
+        super(DictUDS, self).__init__(main_uds)
         backend_config.setdefault('protocol', 'dict')
         self.kvstore = KeyValueStore.instantiate(**backend_config)
     def add_infrastructure(self, static_description):
@@ -418,8 +422,8 @@ class RedisUDS(UDS):
         features if possible/suitable.
     """
 
-    def __init__(self, **backend_config):
-        super(RedisUDS, self).__init__()
+    def __init__(self, main_uds=True, **backend_config):
+        super(RedisUDS, self).__init__(main_uds)
         backend_config.setdefault('protocol', 'redis')
         self.kvstore = KeyValueStore.instantiate(**backend_config)
 
