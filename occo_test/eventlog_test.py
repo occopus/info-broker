55ba5d5a37ed20e936949a2c63114f1d5996132b
Adam Visegradi <visegradi.adam@sztaki.mta.hu>
2015-08-04 10:31:17 +0000

    Add test cases

--- occo_test/eventlog_test.py
+++ occo_test/eventlog_test.py
@@ -28,3 +28,28 @@ class EventLogTest(unittest.TestCase):
         print result
         self.assertIn('timestamp', event)
         self.assertEqual(yaml.load(result), event)
+
+    def test_eli_kw(self):
+        elog = el.EventLog.instantiate('logging', 'occo.test.eventlog')
+        elog.raw_log_event(a=1, b=2, timestamp=0)
+        result = self.stream.getvalue()
+
+        print result
+        res = yaml.load(result)
+        self.assertIn('timestamp', res)
+        self.assertIn('a', res)
+        self.assertIn('b', res)
+        self.assertEquals(res['timestamp'], 0)
+
+    def test_convenience_functions(self):
+        elog = el.EventLog.instantiate('logging', 'occo.test.eventlog')
+        elog.infrastructure_created('infraid1')
+        result = self.stream.getvalue()
+
+        print result
+        res = yaml.load(result)
+        self.assertIn('timestamp', res)
+        self.assertIn('name', res)
+        self.assertIn('infra_id', res)
+        self.assertEquals(res['name'], 'infrastart')
+        self.assertEquals(res['infra_id'], 'infraid1')
