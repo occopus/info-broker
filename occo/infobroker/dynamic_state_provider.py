ab17e9035600726e4ba915e53b35791128f6f60a
Jozsef Kovacs <jozsef.kovacs@sztaki.mta.hu>
2016-01-14 11:15:09 +0100

    Format debug messages

--- occo/infobroker/dynamic_state_provider.py
+++ occo/infobroker/dynamic_state_provider.py
@@ -68,8 +68,8 @@ class DynamicStateProvider(ib.InfoProvider):
             if sv_state != status.READY:
                 afp = main_uds.get_failing_period(instance_data['infra_id'],instance_data['node_id'],True)
                 timeout = instance_data.get('resolved_node_definition',dict()).get('service_health_check',dict()).get('timeout',600)
-		log.warning('Service on node %r/%r is down for %r seconds! (timeout: %r)', 
-				instance_data['infra_id'],instance_data['node_id'], afp, timeout)
+		log.warning('Service on node %r (NodeId: %r) is down for %.3f seconds! (Timeout for restart: %is)', 
+				instance_data.get('resolved_node_definition',dict()).get('name'),instance_data['node_id'], afp, timeout)
                 if afp > timeout:
                     sv_state = status.FAIL
             else:
