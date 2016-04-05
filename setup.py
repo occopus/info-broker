8dead03bb03d0b7e14009cd0bf436992f050fdf2
Jozsef Kovacs <jozsef.kovacs@sztaki.mta.hu>
2016-04-05 16:53:36 +0200

    Prepared for release 1.0

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ from pip.req import parse_requirements
 
 setuptools.setup(
     name='OCCO-InfoBroker',
-    version='0.3.0',
+    version='1.0',
     author='MTA SZTAKI',
     author_email='occopus@lpds.sztaki.hu',
     namespace_packages=['occo'],
